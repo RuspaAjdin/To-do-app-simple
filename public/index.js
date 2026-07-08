969cfabc4970dd380693825588b1554e343d2a23
RuspaAjdin <ajdinruspa@gmail.com>
2026-07-08 16:45:53 +0200

    Finalizes backend finishes full app

--- public/index.js
+++ public/index.js
@@ -1,5 +1,6 @@
 const form = document.getElementById("form-container");
 const input = document.getElementById("task-input");
+const taskContainer = document.getElementById("task-container");
 
 form.addEventListener("submit", async (event) => {
     event.preventDefault();
@@ -12,10 +13,43 @@ form.addEventListener("submit", async (event) => {
         const response = await fetch("/api/task", {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({task: taskValue})
-        })
+            body: JSON.stringify({ task: taskValue })
+        });
+        const data = await response.json();
+        if (!data.success) {
+            throw new Error('Sending data wasnt successful');
+        }
+        input.value = "";
+        displayTasks();
     } catch (err) {
         console.error(err);
     }
 });
 
+async function displayTasks() {
+    try {
+        const response = await fetch('/api/task');
+        const data = await response.json();
+        if (!data.success) {
+            throw new Error('Failed to load tasks');
+        }
+
+        taskContainer.innerHTML = "";
+
+        data.taskData.forEach(task => {
+            const taskDiv = document.createElement('div');
+            taskDiv.classList.add('task-div');
+
+            const taskParagraph = document.createElement('p');
+            taskParagraph.textContent = task.task;
+
+            taskDiv.appendChild(taskParagraph);
+            taskContainer.appendChild(taskDiv);
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+displayTasks();
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import fs from 'node:fs/promises'
+import fs from 'node:fs/promises';
 
 const app = express();
 const PORT = 3000;
@@ -9,20 +9,65 @@ app.use(express.json());
 
 async function checkFile() {
     try {
-        await fs.readFile('storage.json', 'utf-8')
+        await fs.readFile('storage.json', 'utf-8');
     } catch (err) {
         console.warn("First run or file doesnt exist, creating new");
         await fs.writeFile('storage.json', '[]');
     }
 }
 
-app.post("/api/task", async (req, res) => {
-    const task = req.body.task;
+checkFile();
 
-})
+app.post("/api/task", async (req, res) => {
+    try {
+        const task = req.body.task;
+        const taskString = await fs.readFile('storage.json', 'utf-8');
+        const taskArray = JSON.parse(taskString);
+
+        const newTask = {
+            id: Date.now().toString(),
+            task: task
+        };
+
+        taskArray.push(newTask);
+        await fs.writeFile('storage.json', JSON.stringify(taskArray, null, 2));
+
+        res.status(200).json({
+            success: true
+        });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: 'Failed to save task.'
+        });
+    }
+});
+
+app.get('/api/task', async (req, res) => {
+    try {
+        const taskData = await fs.readFile('storage.json', 'utf-8');
+        
+        if (taskData.trim().length == 0) {
+            return res.status(200).json({ success: true, taskData: [] });
+        }
+        
+        const taskArray = JSON.parse(taskData);
+        
+        res.status(200).json({
+            success: true,
+            taskData: taskArray
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: 'Failed to get data'
+        });
+    }
+});
 
 app.listen(PORT, () => {
-    console.log(`Succesfully running server on port ${PORT}`)
-})
-
-checkFile();
+    console.log(`Succesfully running server on port ${PORT}`);
+});
